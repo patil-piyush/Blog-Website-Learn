3bfe9f7456d789b57e090fc1ada40312873ac1ef
Piyush <piyushpatil0031@gmail.com>
2025-03-17 09:53:21 +0530

    added signup page

--- models/user.js
+++ models/user.js
@@ -30,6 +30,21 @@ const userSchema = new Schema({
 }, { timestamps: true });
 
 
+//middleware for crypting the password using salt
+userSchema.pre('save', function(next){
+    const user = this;
+
+    if(!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt).update(user.password).digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+});
+
 const User = model("user", userSchema);
 
 module.exports = User;
